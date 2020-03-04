1c4b8f5c36a9e3b462e8c1f32e2ae977aaedbc76
Anand3891 <anand.imac.sri3@outlook.com>
2020-03-04 00:21:37 +0000

    adasd

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function validate() {
   var pw = document.myform.pword.value;
   var valid = false;
   /*Change the passwords for your class members*/
-  var unArray = ["Archit", "Sreehitha", "Nakul", "Vrinda", "Diya", "Keshav", "Yuvraj", "Lucas", "Eric", "Maanika", "Dhruva", "Anuj", "Nyan", "Roonhi", "Saachi", "Pranesh", "Anand", "Nathan", "Layton", "Kathy", "admin"];
-  var pwArray = [/*Archit*/ "archeetos", /*Sreehitha*/ "6COverlook",/*Nakul*/"debatechamp246", /* xsZ*/"6COverlook", "6COverlook","6COverlook" , "anything", /*Lucas*/"592", /*Eric*/"Menggg", /*Maanika*/"6COverlook", /*Dhruva*/"6COverlook", /*Anuj*/"football123", /*Nyan*/"poopyface", /*Roonhi*/"SRS", /*Saachi*/"tramarat", /*Pranesh*/"justkeepitnormal", /*Anand*/"1141144", /*Nathan*/"nasangswee", /*Layton*/"exmiouy931", /*Kathy*/"6COverlook", /*admin*/"6Bcopies"];
+  var unArray = ["admin"];
+  var pwArray = [/*admin*/"admin"];
   for (var i = 0; i < unArray.length; i++) {
     if ((un == unArray[i]) && (pw == pwArray[i])) {
       valid = true;
